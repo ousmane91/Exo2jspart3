8329c75babc5f5853f973861053102c70d15859c
ousmane91 <contact@ousmangueye.fr>
2019-04-08 16:41:36 +0200

    exo pas fini

---
+++ script.js
@@ -0,0 +1,8 @@
+//fonction pour afficher le texte
+function showtext() {
+ var text = document.getElementById('text').style.display="none";
+}
+//fonction pour masquer le texte
+function hidetext() {
+ var text = document.getElementById('text').style.display="none";
+}
